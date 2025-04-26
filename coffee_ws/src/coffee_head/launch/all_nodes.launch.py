1d3d0970d68f634f85cec57c92f86f4b6ae285cd
MarcoDotIO <Marcus.Arnett10@gmail.com>
2025-04-26 19:01:03 -0400

    fix: update all_node launch for read_write_coffee

--- coffee_ws/src/coffee_head/launch/all_nodes.launch.py
+++ coffee_ws/src/coffee_head/launch/all_nodes.launch.py
@@ -5,10 +5,23 @@ def generate_launch_description():
     """Generate launch description for all coffee_head nodes"""
     
     # Dynamixel SDK read_write_node
-    dynamixel_node = Node(
-        package='dynamixel_sdk_examples',
-        executable='read_write_node',
-        name='dynamixel_node',
+    # dynamixel_node = Node(
+    #     package='dynamixel_sdk_examples',
+    #     executable='read_write_node',
+    #     name='dynamixel_node',
+    #     output='screen',
+    #     emulate_tty=True,
+    #     parameters=[
+    #         {
+    #             'device_name': '/dev/ttyUSB0'
+    #         }
+    #     ]
+    # )
+
+    coffee_dynamixel_node = Node(
+        package='coffee_head',
+        executable='read_write_coffee',
+        name='coffee_dynamixel_node',
         output='screen',
         emulate_tty=True,
         parameters=[
@@ -18,22 +31,30 @@ def generate_launch_description():
         ]
     )
     
-    # # Camera node
-    # camera_node = Node(
-    #     package='coffee_head',
-    #     executable='camera_node',
-    #     name='camera_node',
-    #     output='screen'
-    # )
+    # Camera node
+    camera_node = Node(
+        package='coffee_head',
+        executable='camera_node',
+        name='camera_node',
+        output='screen'
+    )
+
+    # Coffee Buddy State Manager
+    coffee_buddy_state_manager_node = Node(
+        package='coffee_expressions_state_manager',
+        executable='state_manager_node',
+        name='coffee_buddy_state_manager_node',
+        output='screen'
+    )
     
-    # # Head tracking node
-    # head_tracking_node = Node(
-    #     package='coffee_head',
-    #     executable='head_tracking',
-    #     name='head_tracking_node',
-    #     output='screen',
-    #     emulate_tty=True
-    # )
+    # Head tracking node
+    head_tracking_node = Node(
+        package='coffee_head',
+        executable='head_tracking',
+        name='head_tracking_node',
+        output='screen',
+        emulate_tty=True
+    )
     
     # # Eye tracking node
     # eye_tracking_node = Node(
@@ -60,9 +81,10 @@ def generate_launch_description():
     
     # Return launch description
     return LaunchDescription([
-        dynamixel_node,
-        # camera_node,
-        # head_tracking_node,
+        coffee_dynamixel_node,
+        coffee_buddy_state_manager_node,
+        camera_node,
+        head_tracking_node
         # eye_tracking_node,
         # face_recognition_node
     ]) 
